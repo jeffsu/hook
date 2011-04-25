5df4103197d4b99d1096ca27580d86283838d213
Jeff Su <me@jeffsu.com>
2011-04-25 14:26:53 -0700

    spike for new throttling + admin

--- example/simple.js
+++ example/simple.js
@@ -1,24 +1,24 @@
 
-var ProxyServer = require('../lib/hook').ProxyServer;
-var Throttler   = require('../lib/hook').Throttler;
+var hook = require('./lib/hook');
+var http = require('http');
 
-var server    = { port: 8020 };
-var proxy     = { port: 3000 };
+// throttler 10 buckets over 10 seconds
+var throttler = new hook.Throttler(10, 10); 
 
-var nBuckets  = 10;  // this is the length of time... granularity is set to timeframe / nBuckets
-var timeframe = 60;  // timeframe for limit
-var limit     = 50;   // number of request in a given timeFrame
+// proxy
+var proxy = new hook.ProxyServer(3000, 'localhost');
 
-var throttler = new Throttler(nBuckets, timeframe);
+// add ip address filter
+proxy.addFilter(new hook.AddressFilter(throttler, 10));
 
-// filter function
-// return true  to allow proxied request
-// return false to deny it
-function filter(req, res) {
-  // in this case, use ip address as key
-  var key = req.connection.remoteAddress; 
-  throttler.logRequest(key, limit);
-  return throttler.shouldAllow(key);
-};
+// http server listen on port 8080
+http.createServer(function (req, resp) {
+  proxy.proxyRequest(req, resp);
+}).listen(8080);
 
-var server = ProxyServer.start(server, proxy, filter);
+// Run throttle admin on port 8081
+var admin = new hook.ThrottleAdmin(throttler);
+admin.listen(8081);
+
+// flush irrelevant tracking every 10 seconds
+throttler.startFlush(10);
